f4b40c892bebb950fc1f3c3287dcabbe237c1207
giatkt1598 <giatkt1598@gmail.com>
2024-05-06 23:25:27 +0700

    update

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -4,7 +4,7 @@ import { CommonHelper } from '../utilities/common-helper';
 
 export class ProjectService {
   private static readonly TABLE = 'Project';
-  private static readonly DATA_FOLDER = `${app.getPath('appData')}\\RepoConfigManager`;
+  private static readonly DATA_FOLDER = `${app.getPath('appData')}\\repo-configuration-manager`;
   private static readonly DATA_FILE = `${this.DATA_FOLDER}\\data.json`;
 
   static insert(project: Project) {
@@ -28,6 +28,9 @@ export class ProjectService {
   }
 
   static getList() {
+    const dataFromFile = this.loadFromUserData();
+    if (dataFromFile.length > 0) return dataFromFile;
+
     const str = localStorage.getItem(this.TABLE);
     return str ? (JSON.parse(str) as Project[]) : [];
   }
@@ -38,13 +41,26 @@ export class ProjectService {
 
   private static setList(projects: Project[]) {
     localStorage.setItem(this.TABLE, JSON.stringify(projects));
-    this.saveToUserData(JSON.stringify(projects) || '[]');
+    this.saveToUserData(JSON.stringify(projects, null, 4) || '[]');
   }
 
   static saveToUserData(data: string) {
     if (!fs.existsSync(this.DATA_FOLDER)) {
       fs.mkdirSync(this.DATA_FOLDER);
     }
+    localStorage.setItem('APP_DATA_PATH', this.DATA_FOLDER);
     fs.writeFileSync(this.DATA_FILE, data);
+  }
+
+  static loadFromUserData(): Project[] {
+    if (!fs.existsSync(this.DATA_FILE)) {
+      return [];
+    }
+
+    try {
+      return JSON.parse(fs.readFileSync(this.DATA_FILE).toString() || '[]');
+    } catch (error) {
+      return [];
+    }
   }
 }
